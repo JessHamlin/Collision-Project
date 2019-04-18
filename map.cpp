e00e95a84483616db5908d80c62e2e2ae1f2381a
GarlicWaffle <49416720+GarlicWaffle@users.noreply.github.com>
2019-04-17 18:14:03 -0600

    Simple way to make the map
    To do:
    Make a movable character
    Make the quadtree and hashtable way of storing collision data

--- map.cpp
+++ map.cpp
@@ -1,6 +1,8 @@
 #include "map.hpp"
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <sstream>
 using namespace std;
 
 map::map(int x, int y)
@@ -10,16 +12,28 @@ map::map(int x, int y)
     vector<char> temp;
     for (int j = 0; j < x; j++)
     {
-      temp.push_back(178);
+      temp.push_back(' ');//219 = block
     }
     grid.push_back(temp);
   }
   sizeX = x;
   sizeY = y;
 }
+map::map()
+{
+  sizeX = 0;
+  sizeY = 0;
+}
 char map::getVal(int x, int y)
 {
   return grid[x][y];
+}
+void map::setVal(int x, int y, bool v)
+{
+  if (v == false)
+  grid[x][y] = ' ';
+  if (v == true)
+  grid[x][y] = 219;
 }
 void map::print()
 {
@@ -27,13 +41,55 @@ void map::print()
   {
     for (int j = 0; j < sizeX; j++)
     {
-      cout << getVal(i,j) << " ";
+      cout << getVal(i,j);
     }
     cout << endl;
   }
 }
-int main()
+void map::addLine(vector<char> line)
 {
-  map m(2,5);
-  m.print();
+  grid.push_back(line);
+  sizeY++;
+  sizeX = line.size();
+}
+int main(int argc, char const *argv[])
+{
+  cout << "Welcome to Game" << endl;
+  bool fileNameCheck = false;
+  while (fileNameCheck == false)
+  {
+    cout << "Enter map file name" << endl;
+    string fileStr;
+    cin >> fileStr;
+    ifstream file;
+    file.open(fileStr);
+    if (file.is_open())
+    {
+      map m(0,0);
+      fileNameCheck = true;
+      string fileStrB;
+      while (getline(file,fileStr))
+      {
+        vector<char> temp;
+        stringstream ss(fileStr);
+        while (getline(ss,fileStrB,' '))
+        {
+          if (stoi(fileStrB) == 0)
+          {
+            temp.push_back(' ');
+          }
+          if (stoi(fileStrB) == 1)
+          {
+            temp.push_back(219);
+          }
+        }
+        m.addLine(temp);
+      }
+      m.print();
+    }
+    else
+    {
+      cout << "Error opening file" << endl;
+    }
+  }
 }
--- map.hpp
+++ map.hpp
@@ -7,9 +7,12 @@ using namespace std;
 class map
 {
   public:
+    map();
     map(int x, int y);
     char getVal(int x, int y);
     void print();
+    void setVal(int x, int y, bool v);
+    void addLine(vector<char> line);
   private:
     vector<vector<char>> grid;
     int sizeX;
